8f32da6dab25e000cc1c690027d83a804132a641
James-Leroy <jlanvja@gmail.com>
2022-01-23 23:24:57 -0500

    initial

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Card, CardHeader, CardContent, Typography, Grid, Divider} from '@material-ui/core'
+
+
+const Main = () => {
+ return (
+  <div>
+   
+  </div>
+ )
+}
+
+export default Main
